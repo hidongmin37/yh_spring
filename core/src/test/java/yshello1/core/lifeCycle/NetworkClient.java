69c16356917a66c5fce89e8712ea1d1d2ba031c4
jangdm37 <jangdm37@gmail.com>
2023-10-05 23:30:20 +0900

    생성자 호출,종료시 애노테이션

--- core/src/test/java/yshello1/core/lifeCycle/NetworkClient.java
+++ core/src/test/java/yshello1/core/lifeCycle/NetworkClient.java
@@ -1,30 +1,60 @@
 package yshello1.core.lifeCycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+//public class NetworkClient implements InitializingBean, DisposableBean {
 public class NetworkClient {
 
     private String url;
 
 
-
     public void setUrl(String url) {
         this.url = url;
     }
+
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
-        disconnect();
+
     }
+
     // 서비스 시작시 호출
-    public void connect(){
+    public void connect() {
         System.out.println("connect : " + url);
     }
-    public void call(String message){
+
+    public void call(String message) {
         System.out.println("call : " + url + " message : " + message);
     }
 
     //서비스 종료시 호출
-    public void disconnect(){
+    public void disconnect() {
         System.out.println("disconnect : " + url);
+    }
+
+//    @Override //의존관계 주입이 끝나면 호출 InitializingBean
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메세지");
+//    }
+//
+//    @Override // DisposableBean
+//    public void destroy() throws Exception {
+//        disconnect();
+//
+//    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        disconnect();
+
     }
 }
