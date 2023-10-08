b9876ec11aef2b624e7101316a36bb8bf8bedbb8
jangdm37 <jangdm37@gmail.com>
2023-10-08 17:13:06 +0900

    d

--- servlet_mvc/src/main/java/servlet_mvc/servlet/basic/HelloServlet.java
+++ servlet_mvc/src/main/java/servlet_mvc/servlet/basic/HelloServlet.java
@@ -13,7 +13,15 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+        String userName = request.getParameter("user_name");
+        System.out.println("request.getParameter(\"user_name\") = " + userName);
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + userName);
+
     }
 }
