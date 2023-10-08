d2a2b5a3f7c0dac8ef02672cdec15b7eeffd4aaf
jangdm37 <jangdm37@gmail.com>
2023-10-08 16:35:55 +0900

    servlet with yh start

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -1,13 +1,13 @@
-package hello.servelt;
+package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServeltApplication {
+public class ServletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServeltApplication.class, args);
+		SpringApplication.run(ServletApplication.class, args);
 	}
 
 }
