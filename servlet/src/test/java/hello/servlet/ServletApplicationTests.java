e7d3d3d5b46594fc6cc49c052401fba23576b078
jangdm37 <jangdm37@gmail.com>
2023-10-08 16:41:50 +0900

    servlet with yh start

--- servlet/src/test/java/hello/servlet/ServletApplicationTests.java
+++ servlet/src/test/java/hello/servlet/ServletApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServeltApplicationTests {
+class ServletApplicationTests {
 
 	@Test
 	void contextLoads() {
