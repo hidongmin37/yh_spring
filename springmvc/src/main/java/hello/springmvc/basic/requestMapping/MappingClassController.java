947794a03d99f033d2eb99713f2ce71d38e9b7d2
jangdm37 <jangdm37@gmail.com>
2023-10-12 21:59:09 +0900

    get,post mapping

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingClassController.java
@@ -0,0 +1,36 @@
+package hello.springmvc.basic.requestMapping;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingClassController {
+
+    @GetMapping("/mapping/users")
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping("/mapping/users")
+    public String addUser(){
+        return "post user";
+    }
+
+    @GetMapping("/mapping/users/{userId}")
+    public String findUser(@PathVariable String userId){
+        return "post userId = " + userId;
+    }
+
+    @PatchMapping("/mapping/users/{userId}")
+    public String updateUser(@PathVariable String userId){
+        return "patch userId = " + userId;
+    }
+
+
+    @DeleteMapping("/mapping/users/{userId}")
+    public String deleteUser(@PathVariable String userId){
+        return "delete userId = " + userId;
+    }
+
+
+}
