7697fb5b4c926d9f79dd237eeb0273529eda4605
jangdm37 <jangdm37@gmail.com>
2023-10-18 00:23:34 +0900

    체크 박스

--- form-start/src/main/java/hello/itemservice/domain/item/Item.java
+++ form-start/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
@@ -10,6 +12,9 @@ public class Item {
     private Integer price;
     private Integer quantity;
     private Boolean open; // 판매 여부
+    private List<String> regions; // 등록 지역
+    private ItemType itemType;
+    private String deliveryCode; //배송방식
 
     public Item() {
     }
