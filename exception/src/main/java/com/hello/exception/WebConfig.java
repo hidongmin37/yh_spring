4b5fee41b4a3d4235b606ac6030bef1840238952
jangdm37 <jangdm37@gmail.com>
2023-10-25 02:01:21 +0900

    예외 처리 페이지

--- exception/src/main/java/com/hello/exception/WebConfig.java
+++ exception/src/main/java/com/hello/exception/WebConfig.java
@@ -1,18 +1,27 @@
 package com.hello.exception;
 
 import com.hello.exception.filter.LogFilter;
+import com.hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterRegistration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override //setDispatcherTypes은 사용할 수 없고, excludePatterns가능함.
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/*")
+                .excludePathPatterns("/css/**", "*.ico", "/error","/error-page/**");
+    }
 
-    @Bean // 이 필터는 에러랑 리퀘스트시에 호출이 된다.
+//    @Bean // 이 필터는 에러랑 리퀘스트시에 호출이 된다., DispatcherType.REQUEST이 기본임
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
