a4b02582dfa1ad26be3fb41048d0251c00432e7e
jangdm37 <jangdm37@gmail.com>
2023-10-26 22:42:09 +0900

    .

--- typeConverter/src/main/java/com/hello/typeConverter/WebConfig.java
+++ typeConverter/src/main/java/com/hello/typeConverter/WebConfig.java
@@ -4,6 +4,7 @@ import com.hello.typeConverter.converter.IntegerToStringConverter;
 import com.hello.typeConverter.converter.IpPortToStringConverter;
 import com.hello.typeConverter.converter.StringToIntegerConverter;
 import com.hello.typeConverter.converter.StringToIpPortConverter;
+import com.hello.typeConverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,11 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+    @Override//스프링이 들고있는 기본적인 서비스에 등록이 됨.
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+
+        //우선순위때문에 주석처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
---
+++ typeConverter/src/test/java/com/hello/typeConverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,33 @@
+package com.hello.typeConverter.formatter;
+
+import com.hello.typeConverter.converter.IpPortToStringConverter;
+import com.hello.typeConverter.converter.StringToIpPortConverter;
+import com.hello.typeConverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+public class FormattingConversionServiceTest {
+
+
+    @Test
+    void formattingConversionService(){
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        // 컨버터 등록
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+        //포멧터 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        //컨버터 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        Assertions.assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        //포메터 사용
+        String convert = conversionService.convert(1000, String.class);
+        Assertions.assertThat(convert).isEqualTo("1,000");
+        Long convert2 = conversionService.convert( "1,000",Long.class);
+        Assertions.assertThat(convert2).isEqualTo(1000L);
+
+    }
+}
