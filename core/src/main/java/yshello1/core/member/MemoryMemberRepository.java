5a7683304c56fe8d39ea2fbe90ba70429132a620
jangdm37 <jangdm37@gmail.com>
2023-11-02 00:37:06 +0900

    .

--- core/src/main/java/yshello1/core/member/MemoryMemberRepository.java
+++ core/src/main/java/yshello1/core/member/MemoryMemberRepository.java
@@ -1,6 +1,6 @@
 package yshello1.core.member;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;;
 
 import java.util.HashMap;
 import java.util.Map;
