bcb3d208d6e48b33faba82020ef3434885eff541
jangdm37 <jangdm37@gmail.com>
2023-11-20 22:26:59 +0900

    트랜잭션

---
+++ db_source/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,64 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+
+    /*
+    * memberService     @Transactional :OFF
+    * memberRepository  @Transactional :ON
+    * logRepository     @Transactional :ON
+    * */
+    @Test
+    void outerTxOff_success() {
+        //given
+        String username = "outerTxOff_success";
+
+        //when : 모든 데이터가 정상 저장된다.
+        memberService.joinV1(username);
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+
+    }
+
+    /*
+     * memberService     @Transactional :OFF
+     * memberRepository  @Transactional :ON
+     * logRepository     @Transactional :ON Exception이 여기서 날아옴
+     * */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when : 모든 데이터가 정상 저장된다.
+//        memberService.joinV1(username);
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+
+    }
+
+}
