644bbcf6e95e58e242ab495d85071d0ac1421b50
jangdm37 <jangdm37@gmail.com>
2023-12-10 00:01:03 +0900

    수정

---
+++ java-basic/src/class1/ClassStart1.java
@@ -0,0 +1,11 @@
+package class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String studentName = "학생1";
+        int studenttAge = 15;
+        String studentName = "학생1";
+
+    }
+}
