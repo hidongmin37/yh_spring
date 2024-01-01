6ad7f5b8224ae1495e7085dc0c359bfc135f51a7
jangdm37 <jangdm37@gmail.com>
2024-01-02 02:32:15 +0900

    servlet 구현

--- aop/src/test/java/hello/aop/proxyvs/code/ProxyDIAspect.java
+++ aop/src/test/java/hello/aop/proxyvs/code/ProxyDIAspect.java
@@ -2,9 +2,17 @@ package hello.aop.proxyvs.code;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
+@Aspect
 public class ProxyDIAspect {
+
+    @Before("execution(* hello.aop..*.*(..))")
+    public void doTrace(JoinPoint joinPoint) {
+        log.info("[proxyDIAdvice] {}", joinPoint.getSignature());
+    }
 }
