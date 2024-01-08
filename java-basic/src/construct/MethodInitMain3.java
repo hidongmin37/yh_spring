cd41c205452af778baeee5de26372b312f51e92e
jangdm37 <jangdm37@gmail.com>
2024-01-09 01:39:29 +0900

    .

---
+++ java-basic/src/construct/MethodInitMain3.java
@@ -0,0 +1,23 @@
+
+package construct;
+
+public class MethodInitMain3 {
+
+    public static void main(String[] args) {
+        MemberInit member1 = new MemberInit();
+        member1.MemberInit("user1", 15, 90);
+
+
+
+        MemberInit member2 = new MemberInit();
+        member2.MemberInit("user2", 16, 80);
+
+        MemberInit[] members = {member1, member2};
+
+        for (MemberInit m : members) {
+            System.out.println("이름: " + m.name + " 나이: " + m.age + " 성적: " +m.grade);
+        }
+    }
+
+
+}
