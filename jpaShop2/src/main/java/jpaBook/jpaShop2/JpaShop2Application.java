e33c2c75a37bfb455e2e2b58fa1b14ea975bb504
jangdm37 <jangdm37@gmail.com>
2024-02-10 23:06:25 +0900

    spring boot jpa apply

---
+++ jpaShop2/src/main/java/jpaBook/jpaShop2/JpaShop2Application.java
@@ -0,0 +1,13 @@
+package jpaBook.jpaShop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaShop2Application.class, args);
+	}
+
+}
---
+++ jpaShop2/src/test/java/jpaBook/jpaShop2/JpaShop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpaBook.jpaShop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
