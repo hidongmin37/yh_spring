b240f14197fd00475e200d3037d39f9e1d7c6046
jangdm37 <jangdm37@gmail.com>
2024-04-18 00:10:29 +0900

    DiscountService

---
+++ java-basic/src/lang/math/MathMain.java
@@ -0,0 +1,10 @@
+package lang.math;
+
+public class MathMain {
+
+    public static void main(String[] args) {
+        Math.max(10, 20);
+        Math.min(10, 20);
+        Math.random();
+    }
+}
