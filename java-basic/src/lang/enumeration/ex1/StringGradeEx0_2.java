950506d05b1a82600c3ec3ca938aa24e01d9e3de
jangdm37 <jangdm37@gmail.com>
2024-04-18 23:43:40 +0900

    new

--- java-basic/src/lang/enumeration/ex1/StringGradeEx0_2.java
+++ java-basic/src/lang/enumeration/ex1/StringGradeEx0_2.java
@@ -9,7 +9,7 @@ public class StringGradeEx0_2 {
         int basic = discountService.discount(StringGrade.BASIC, price);
         int gold = discountService.discount(StringGrade.GOLD, price);
         int diamond = discountService.discount(StringGrade.DIAMOND, price);
-
+//
         System.out.println("basic = " + basic);
         System.out.println("diamond = " + diamond);
         System.out.println("gold = " + gold);
