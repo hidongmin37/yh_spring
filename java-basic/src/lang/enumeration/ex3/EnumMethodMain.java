f66d58faeb62c9d8e203f2e7cbc8e2beae72579e
jangdm37 <jangdm37@gmail.com>
2024-05-04 01:24:08 +0900

    .

---
+++ java-basic/src/lang/enumeration/ex3/EnumMethodMain.java
@@ -0,0 +1,19 @@
+package lang.enumeration.ex3;
+
+import java.util.Arrays;
+
+public class EnumMethodMain {
+
+    public static void main(String[] args) {
+        //모든 ENUM 반환
+        Grade[] values = Grade.values();
+        System.out.println("values = " + Arrays.toString(values));
+        for (Grade value : values) {
+            System.out.println("value.name() = " + value.name()+"order " + value.ordinal());
+        }
+
+        String input = "GOLD";
+        Grade gold = Grade.valueOf(input);
+        System.out.println(gold);
+    }
+}
