9ba6e82766f52ccb79b0c857fc33028ce38fb665
jangdm37 <jangdm37@gmail.com>
2024-05-08 00:06:00 +0900

    .

--- java-basic/src/coding_test/Test.java
+++ java-basic/src/coding_test/Test.java
@@ -2,11 +2,11 @@ package coding_test;
 
 import java.time.*;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Test {
 
     public static void main(String[] args) {
-
 
     }
 }
