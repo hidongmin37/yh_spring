d58553961cd2dabef1be08563b9cf9a3ad73162f
jangdm37 <jangdm37@gmail.com>
2024-11-24 18:02:01 +0900

    java list

--- java-basic/src/main/java/com/java_basic/collection/list/BatchProcessorMain.java
+++ java-basic/src/main/java/com/java_basic/collection/list/BatchProcessorMain.java
@@ -3,7 +3,10 @@ package com.java_basic.collection.list;
 public class BatchProcessorMain {
 
     public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        MyLinkedList<Integer> list2 = new MyLinkedList<>();
 
-        
+        BatchProcessor processor = new BatchProcessor(list2);
+        processor.logic(50_000);
     }
 }
