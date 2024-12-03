84a2d4a264967cd59bc823bbbd7b3cf63dd0e740
jangdm37 <jangdm37@gmail.com>
2024-12-03 17:54:01 +0900

    hashset

---
+++ java-basic/src/main/java/com/java_basic/collection/set/MyHashSetV3Main.java
@@ -0,0 +1,20 @@
+package com.java_basic.collection.set;
+
+public class MyHashSetV3Main {
+
+    public static void main(String[] args) {
+        //중복 등록
+        MyHashSetV3<String> set = new MyHashSetV3<>(10);
+        set.add("A");
+        set.add("B");
+        set.add("C");
+
+        System.out.println(set);
+
+        // 검색
+        String searchValue = "A";
+        System.out.println("set.contains(searchValue) = " + set.contains(searchValue));
+
+
+    }
+}
---
+++ java-basic/src/main/java/com/java_basic/collection/set/MySet.java
@@ -0,0 +1,8 @@
+package com.java_basic.collection.set;
+
+public interface MySet<E> {
+
+    boolean add(E element);
+    boolean remove(E value);
+    boolean contains(E value);
+}
