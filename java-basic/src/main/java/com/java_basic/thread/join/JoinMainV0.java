bd3c246219c79d36784c2ea869a5565304c935db
jangdm37 <jangdm37@gmail.com>
2025-01-14 18:33:36 +0900

    thread 고급

---
+++ java-basic/src/main/java/com/java_basic/thread/join/JoinMainV0.java
@@ -0,0 +1,30 @@
+package com.java_basic.thread.join;
+
+import com.java_basic.thread.util.MyLogger;
+
+import static com.java_basic.thread.util.ThreadUtils.sleep;
+
+public class JoinMainV0 {
+    public static void main(String[] args) {
+        MyLogger.log("Start");
+        Thread thread1 = new Thread(new Job(), "thread-1");
+        Thread thread2 = new Thread(new Job(), "thread-2");
+
+        thread1.start();
+        thread2.start();
+
+        MyLogger.log("End");
+
+    }
+
+    static class Job implements Runnable {
+        @Override
+        public void run() {
+            MyLogger.log("작업 시작");
+
+            sleep(2000);
+
+            MyLogger.log("작업 종료");
+        }
+    }
+}
---
+++ java-basic/src/main/java/com/java_basic/thread/join/JoinMainV1.java
@@ -0,0 +1,61 @@
+package com.java_basic.thread.join;
+
+import com.java_basic.thread.util.MyLogger;
+
+import static com.java_basic.thread.util.ThreadUtils.sleep;
+
+public class JoinMainV1 {
+    public static void main(String[] args) throws InterruptedException {
+        MyLogger.log("Start");
+        SumTask sumTask1 = new SumTask(1, 50);
+        SumTask sumTask2 = new SumTask(51, 100);
+
+        Thread thread1 = new Thread(sumTask1, "sumTask1");
+        Thread thread2 = new Thread(sumTask2, "sumTask2");
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        MyLogger.log("task1.result = " + sumTask1.result);
+        MyLogger.log("task2.result = " + sumTask2.result);
+
+        int sum = sumTask1.result + sumTask2.result;
+        MyLogger.log("sum = " + sum);
+        MyLogger.log("End");
+
+    }
+
+    static class SumTask implements Runnable {
+        int startValue;
+        int endValue;
+        int result = 0;
+
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        public void run() {
+            MyLogger.log("작업 시작");
+            Thread sumThread = Thread.currentThread();
+            if (sumThread.getName().equals("sumTask1")) {
+                sleep(2000);
+            } else {
+                sleep(6000);
+            }
+
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+
+            this.result = sum;
+            MyLogger.log("작업 종료");
+        }
+    }
+}
