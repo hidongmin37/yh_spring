4016394592eef72f0f06a316a0d09abcc388b957
jangdm37 <jangdm37@gmail.com>
2025-01-21 21:07:48 +0900

    thread

--- java-basic/src/main/java/com/java_basic/thread/volitile1/VolatileFlagMain.java
+++ java-basic/src/main/java/com/java_basic/thread/volitile1/VolatileFlagMain.java
@@ -10,12 +10,12 @@ public class VolatileFlagMain {
     public static void main(String[] args) {
         MyTask task = new MyTask();
         Thread thread = new Thread(task, "work");
-        log("runFlag" + task.runFlag);
+        log("runFlag = " + task.runFlag);
         thread.start();
         sleep(1000);
         log("runFlag를 false로 변경");
         task.runFlag = false;
-        log("runFlag" + task.runFlag);
+        log("runFlag = " + task.runFlag);
         log("main 종료");
 
     }
@@ -27,7 +27,10 @@ public class VolatileFlagMain {
         public void run() {
             log("task 시작");
             while (runFlag) {
-                // runFlag가 false가 되면 while문을 빠져나온다.
+                // 컨텍스트 스위칭이 일어나지 않을 경우에는 값이 바로 반영안됨.
+//                System.out.println("hello");
+                // 출력을 하게 되면, 컨텍스트 스위칭이 발생하여 runFlag가 false가 되었을때,
+                // while문을 빠져나온다.
             }
             log("task 종료");
         }
